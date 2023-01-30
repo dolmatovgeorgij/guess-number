9d81d3caa625cb072fc162c5bc337164aa5ac02f
«George» <dolmatovgeorgij0@gmail.com>
2023-01-31 00:58:19 +0530

    🐛

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 let btn = document.querySelector("#btn-guess");
 let resault = document.querySelector("#resault-count-out");
-let help = document.querySelector("#help-string");
+let help = document.querySelector("#resault-help-out");
+let input = document.querySelector('#field-input')
 
-console.log(btn);
-console.log(help);
-console.log(resault);
 
-let userNum = 5;
-let randomNum = Math.floor(Math.random() * 100);
 
-function guess(num) {
-  if (userNum > num) {
-    console.log("многовато будет... ");
-  } if (userNum < num) {
-    console.log("маловато будет... ");
+let userNum = 0;
+let randomNum = 1 + Math.floor(Math.random() * 100);
+
+btn.onclick = function guess(num) {
+  userNum = input.value;
+  if (userNum > randomNum) {
+    help.textContent = "многовато будет... ";
+  } else if (userNum < num) {
+    help.textContent = "маловато будет... ";
   } else {
-    console.log("вы угадали салам алейкум");
+    help.textContent = "вы угадали салам алейкум";
   }
 
 };
 
-guess(10);
