0342cf1e010b46138d505ea7c11c949152154d75
«George» <dolmatovgeorgij0@gmail.com>
2023-02-01 17:27:21 +0530

    🐛 -fix доступ к элементам - fix поздравлениея при окончании игры

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 let btn = document.querySelector("#btn-guess");
 let resault = document.querySelector("#resault-count-out");
 let help = document.querySelector("#resault-help-out");
-let input = document.querySelector('#field-input')
+let input = document.querySelector("#field-text")
+
 
 
 
 let userNum = 0;
 let randomNum = 1 + Math.floor(Math.random() * 100);
 
-btn.onclick = function guess(num) {
+btn.onclick = function guess(evt) {
   userNum = input.value;
+
+  console.log(userNum);
+
   if (userNum > randomNum) {
     help.textContent = "многовато будет... ";
-  } else if (userNum < num) {
+  } else if (userNum < randomNum) {
     help.textContent = "маловато будет... ";
   } else {
-    help.textContent = "вы угадали салам алейкум";
+    help.textContent = "угадали, вы большой молодeц", alert("ура вы угадали!");
   }
 
 };
